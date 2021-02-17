567901f8200ecd8e569522d1a4c28f78f38325d7
Caroline Sih <sih.caroline@gmail.com>
2021-02-16 23:08:19 -0800

    init commit

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,5 +9,10 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// your code here
+	$(".likeBtn").click(clickLikeBtn);
+}
+
+function clickLikeBtn() {
+	ga('create', 'UA-189961672-1', 'auto');
+	ga('send', 'event', 'like', 'click');
 }
